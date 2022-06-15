4734f2a6c30bdcb586052f02785aedfdc26c96c8
Juan-lag <celuar12@gmail.com>
2022-06-15 17:06:47 -0500

    funciona. Logra buscar en le json

--- src/views/search/comopents/SearchResults/index.js
+++ src/views/search/comopents/SearchResults/index.js
@@ -1,39 +1,14 @@
-import SearchResultsItems from "./"
-export default function SearchResults({results,isSearching}){ 
-//     console.log("valor:",results.id);
-//     return (
+import SearchResultsItem from "./SearchResultsItem";
 
-//         <div>
-//             hola: {results.id}
-
-// {results?.map((value)=>{
-
-// return (
-     
-//     <div key={value.id}>
-
-//         <p>{value.name}</p>
-//         <p>{value.emai}</p>
-//     </div>
-// );
-
-// })}        </div>
-//     );
-// }
-console.log("hola:",results);
-return (
+export default function SearchResults({ results, isSearching }) {
+  return (
     <div style={{ 
         width: "100%",
         padding: "0rem 2rem 0rem 2rem",
     }}>
       {!results.length && isSearching && <p>No existen resultados</p>}
       {results?.map((value) => 
-        <SearchResultsItems key={value.id} {...value} />)}
+        <SearchResultsItem key={value.id} {...value} />)}
     </div>
   );
 }
-
-
-// 
-
-// export default function SearchResults({ results, isSearching }) {
--- src/views/search/index.js
+++ src/views/search/index.js
@@ -1,6 +1,4 @@
-
-
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import SearchBox from "./comopents/SearchBox";
 
 import "./style.css";
@@ -11,32 +9,30 @@ export default function  Search(){
 
     const [isAtTop,setIsAtTop]=useState(false);
     const [userData,setUserData]=useState(data)
-    const [results,setResults]=useState([]);
+    const [results, setResults] = useState([]);
 
     const handleSearchClick=(searchText)=>{
         setIsAtTop(true);
-        if(userData?.length){            
+        if(userData?.length){
+            
+            
+
         // setIsAtTop(!isAtTop);
-        searchText=searchText.toLocaleLowerCase()
-        const filterData=userData.filter((value)=>{
-                    (value.name.toLocaleLowerCase().includes(searchText) ||
+        searchText=searchText.toLocaleLowerCase();
+        const filterData=userData.filter((value)=>(value.name.toLocaleLowerCase().includes(searchText)  ||
                     value.email.toLocaleLowerCase().includes(searchText)||
                     value.username.toLocaleLowerCase().includes(searchText)||
-                    value.phone.toLocaleLowerCase().includes(searchText));
-        });
+                    value.phone.toLocaleLowerCase().includes(searchText)
+        ));
         setResults(filterData);
-        console.log("valor1: ",filterData);
-        console.log("talvez: ",results);
-
-        
         }
     };
     const HandleClose= ()=>{
         setIsAtTop(!isAtTop);
         setResults([]);
     };
-    console.log(results.id)
-    console.log("lo",results.length);
+     
+    // console.log("lo",results.length);
     
 
     return(
